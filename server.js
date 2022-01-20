6b825a0191a72eb28d139891deb66f7f2c2a5423
Loic <dark.evengyl@gmail.com>
2022-01-20 13:44:51 +0100

    avance express

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const { nextTick } = require("process")
 const port = process.env.PORT || 3000
 const app = express()
 
-const dbUsers = require("./db.json")
+let dbUsers = require("./db.json")
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true}))
 
 /*
 Users
@@ -32,7 +37,10 @@ Liste des statuts http important
 */
 
 app.get("/", (req, res) => {
-    res.end("<h1>Accueil</h1>")
+    res.send(`
+        <a href="/api/users">Get all de tous les users : localhost:3000/api/users</a>
+        <a href="/api/users/1">Get one user by id (1) : localhost:3000/api/users/1</a>
+    `)
 })
 
 
@@ -43,15 +51,108 @@ app.get("/api/users/", (req, res) => {
 
     //res.end()
 
-    //res.json(dbUsers)
+    res.json(dbUsers)
 
     //res.redirect("/")
 
     //res.render("<h1>rendu de template html</h1>") --> attention, il faut avoir configurer un moteur de rendu
 
-    res.send("<h1>Accueil</h1>")
+    //res.send("<h1>Accueil</h1>")
 
 })
+
+app.get("/api/users/:id", (req, res, next) => {
+
+    
+    if(parseInt(req.params.id, 10))
+    {
+        let id = parseInt(req.params.id, 10)
+
+        let user = dbUsers.filter(user => user.id == id)
+
+        console.log(user)
+        if(user.length == 0)
+            next()
+        else
+            res.json(user[0])
+    }
+    else{
+        next()
+    }
+})
+
+
+app.post("/api/users", (req, res, next) => {
+   
+    if(req.body.name && req.body.lastName && req.body.age && req.body.techFav)
+    {
+        console.log("mon objet est ok")
+        let newId = dbUsers.length + 1
+        let newUser = req.body
+        newUser.id = newId
+
+        dbUsers.push(newUser)
+        res.end()
+    }
+    else{
+        res.status("500").send("Objet User non conforme")
+    }
+})
+
+
+app.put("/api/users/:id", (req, res, next) => {
+    
+
+
+    if(parseInt(req.params.id), 10)
+    {
+        let id = parseInt(req.params.id, 10)
+
+        if(req.body.name && req.body.lastName && req.body.age && req.body.techFav)
+        {
+            dbUsers = dbUsers.map((item) => {
+
+                if(item.id == id)
+                {
+                    item.name = req.body.name
+                    item.lastName = req.body.lastName
+                    item.age = req.body.age
+                    item.techFav = req.body.techFav
+                }
+                
+                return item
+            })
+
+            res.end()
+
+        }
+        else{
+            res.status("500").send("Objet User non conforme")
+        }
+    }
+    else{
+        res.status("500").send("ID User non conforme")
+    }
+})
+
+
+app.delete("/api/users/:id", (req, res, next) => {
+    if(parseInt(req.params.id), 10)
+    {
+        let id = parseInt(req.params.id, 10)
+        dbUsers = dbUsers.filter((user) => {
+            if(user.id != id)
+                return user
+        })
+
+        res.end()
+    }
+    else{
+        res.status("500").send("ID User non conforme")
+    }
+})
+
+
 
 
 app.all("*", (req, res) => {
